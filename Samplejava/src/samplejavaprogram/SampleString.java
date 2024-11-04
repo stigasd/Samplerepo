fa7c5169e5c1497e0c94965774e68710a908612b
arun <arundarsh62@gmail.com>
2024-11-04 11:25:14 +0530

    intial push

---
+++ Samplejava/src/samplejavaprogram/SampleString.java
@@ -0,0 +1,17 @@
+package samplejavaprogram;
+
+public class SampleString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Hello";
+		String  s2=s1.concat("welcome");
+		char s3=s1.charAt(1);
+		int s4=s1.length();
+		System.out.println(s3);
+		
+		System.out.println(s2);
+System.out.println(s4);
+	}
+
+}
